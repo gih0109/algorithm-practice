68882df57209a7dfa6c9d9a2e820f281760dd50f
O2rabbit <gih5444@naver.com>
2022-07-18 15:02:47 +0900

    22.07.18

--- 프로그래머스/고득점 Kit/동적계획법/도둑질.py
+++ 프로그래머스/고득점 Kit/동적계획법/도둑질.py
@@ -1,38 +1,53 @@
-# 수정필요
-
 def solution(money):
     n = len(money)
-    dy = [[0, 0] for _ in range(n)]
-    dy[0] = [money[0], 0]
-    dy[1] = [money[1], 1]
-    dy[2] = [money[0] + money[2], 0]
+    dy_1 = [[0, 0] for _ in range(n)] # 정방향 더함
+    dy_2 = [[0, 0] for _ in range(n)] # 역방향 더함
+    dy_1[0] = [money[0], 0]
+    dy_1[1] = [money[1], 1]
+    dy_2[-1] = [money[-1], 0]
+    dy_2[-2] = [money[-2], 1]
 
-    max_val = max(money[0], money[1], money[2])
-    if n > 3:
-        max_val = max(dy[0][0], dy[1][0], dy[2][0])
+    max_val = max(dy_1[0][0], dy_1[1][0], dy_2[-1][0], dy_2[-2][0])
 
-        for i in range(3, n):
-            if i == n - 1:
-                tmp = []
-                if dy[i-2][1]:
-                    tmp.append(dy[i-2][0] + money[i])
-                if dy[i-3][1]:
-                    tmp.append(dy[i-3][0] + money[i])
-                if tmp:
-                    dy[i] = [max(tmp), 1]
-
-            else:    
-                if dy[i-2][0] > dy[i-3][0]:
-                    dy[i] = [dy[i-2][0] + money[i], dy[i-2][1]]
+    for i in range(2, n):
+        if i == n - 1:
+            if dy_1[i][1] == 1:
+                continue
+            else:
+                if dy_1[i-2][0] > dy_1[i-3][0]:
+                    dy_1[i] = [dy_1[i-2][0] + money[i], dy_1[i-2][1]]
                 else:
-                    dy[i] = [dy[i-3][0] + money[i], dy[i-3][1]]
+                    dy_1[i] = [dy_1[i-3][0] + money[i], dy_1[i-3][1]]
 
-            if dy[i][0] > max_val:
-                max_val = dy[i][0]
+            if dy_2[i-1][1] == 1:
+                continue
+            else:
+                if dy_2[-i+1][0] > dy_2[-i+2][0]:
+                    dy_2[-i-1] = [dy_2[-i+1][0] + money[-i-1], dy_1[-i+1][1]]
+                else:
+                    dy_2[-i-1] = [dy_2[-i+2][0] + money[-i-1], dy_1[-i+2][1]]
+
+        else:
+            # dy_1    
+            if dy_1[i-2][0] > dy_1[i-3][0]:
+                dy_1[i] = [dy_1[i-2][0] + money[i], dy_1[i-2][1]]
+            else:
+                dy_1[i] = [dy_1[i-3][0] + money[i], dy_1[i-3][1]]
+            # dy_2
+            if dy_2[-i+1][0] > dy_2[-i+2][0]:
+                dy_2[-i-1] = [dy_2[-i+1][0] + money[-i-1], dy_1[-i+1][1]]
+            else:
+                dy_2[-i-1] = [dy_2[-i+2][0] + money[-i-1], dy_1[-i+2][1]]
+
+        if dy_1[i][0] > max_val:
+            max_val = dy_1[i][0]
+
+        if dy_2[-i-1][0] > max_val:
+            max_val = dy_2[-i-1][0]
 
     return max_val
 
 
 if __name__ == "__main__":
-    money = [2, 8, 20, 6, 13, 1, 15]
+    money = [15, 2, 8, 20, 6, 13, 1]
     print(solution(money))
