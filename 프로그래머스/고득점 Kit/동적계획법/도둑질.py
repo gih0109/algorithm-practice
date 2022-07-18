f3705bd4d7bae0845f33dad69f53bdcd9dc76b29
O2rabbit <gih5444@naver.com>
2022-07-18 16:17:53 +0900

    22.07.18

--- 프로그래머스/고득점 Kit/동적계획법/도둑질.py
+++ 프로그래머스/고득점 Kit/동적계획법/도둑질.py
@@ -10,35 +10,33 @@ def solution(money):
     max_val = max(dy_1[0][0], dy_1[1][0], dy_2[-1][0], dy_2[-2][0])
 
     for i in range(2, n):
-        if i == n - 1:
-            if dy_1[i][1] == 1:
-                continue
-            else:
-                if dy_1[i-2][0] > dy_1[i-3][0]:
-                    dy_1[i] = [dy_1[i-2][0] + money[i], dy_1[i-2][1]]
-                else:
-                    dy_1[i] = [dy_1[i-3][0] + money[i], dy_1[i-3][1]]
+        # dy_1    
+        if dy_1[i-2][0] > dy_1[i-3][0]:
+            tmp_1 = [dy_1[i-2][0] + money[i], dy_1[i-2][1]]
+        else:
+            tmp_1 = [dy_1[i-3][0] + money[i], dy_1[i-3][1]]
+        # dy_2
+        if dy_2[-i+1][0] > dy_2[-i+2][0]:
+            tmp_2 = [dy_2[-i+1][0] + money[-i-1], dy_1[-i+1][1]]
+        else:
+            tmp_2 = [dy_2[-i+2][0] + money[-i-1], dy_1[-i+2][1]]
 
-            if dy_2[i-1][1] == 1:
+        if i == n - 1:
+            if tmp_1[1] == 0:
                 continue
             else:
-                if dy_2[-i+1][0] > dy_2[-i+2][0]:
-                    dy_2[-i-1] = [dy_2[-i+1][0] + money[-i-1], dy_1[-i+1][1]]
-                else:
-                    dy_2[-i-1] = [dy_2[-i+2][0] + money[-i-1], dy_1[-i+2][1]]
+                dy_1[i] = tmp_1
+
+            if tmp_2[1] == 0:
+                continue
+            else:
+                dy_2[-i-1] = tmp_2
 
         else:
-            # dy_1    
-            if dy_1[i-2][0] > dy_1[i-3][0]:
-                dy_1[i] = [dy_1[i-2][0] + money[i], dy_1[i-2][1]]
-            else:
-                dy_1[i] = [dy_1[i-3][0] + money[i], dy_1[i-3][1]]
-            # dy_2
-            if dy_2[-i+1][0] > dy_2[-i+2][0]:
-                dy_2[-i-1] = [dy_2[-i+1][0] + money[-i-1], dy_1[-i+1][1]]
-            else:
-                dy_2[-i-1] = [dy_2[-i+2][0] + money[-i-1], dy_1[-i+2][1]]
+            dy_1[i] = tmp_1
+            dy_2[-i-1] = tmp_2
 
+        # 최대값 갱신
         if dy_1[i][0] > max_val:
             max_val = dy_1[i][0]
 
