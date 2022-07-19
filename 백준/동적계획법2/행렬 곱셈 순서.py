3f580d1ef20e7a8a7fd9abc1de7a6b71e07f47e2
O2rabbit <gih5444@naver.com>
2022-07-19 15:02:01 +0900

    22.07.19

--- 백준/동적계획법2/행렬 곱셈 순서.py
+++ 백준/동적계획법2/행렬 곱셈 순서.py
@@ -1,10 +1,26 @@
 import sys
-sys.stdin = open(r"동적계획법2\test.txt", "r")
-# input = sys.stdin.readline
+sys.stdin = open(r"백준\동적계획법2\test.txt", "r")
+input = sys.stdin.readline
 
 n = int(input())
-arr  = [list(map(int, input())) for _ in range(n)]
+matrix  = [list(map(int, input().split())) for _ in range(n)]
 # sum_arr = [0] * n
 
-dy = [[0] * (n+1) for _ in range(n+1)]
+dy = [[0] * n for _ in range(n)]
+
+for i in range(1, n): # 몇번쨰 대각선 
+    for j in range(n-i): # 대각선에서 몇 번째 열
+
+        min_val = sys.maxsize
+        for k in range(j, j+i):
+            tmp = dy[j][k] + dy[k+1][i+j] + matrix[j][0] * matrix[k][1] * matrix[i+j][1]
+
+            if tmp < min_val:
+                min_val = tmp
+        
+        dy[j][j+i] = min_val
+
+print(dy[0][-1])
+
+
 
